3a0413c7051484b604aef7b11c4f78a5cfe4f1a1
literale <43133944+literale@users.noreply.github.com>
2018-11-03 13:03:06 +0600

    Добавлен ход компьютера

--- TicToc.py
+++ TicToc.py
@@ -1,3 +1,5 @@
+import random
+
 def print_field(game_field):
     line_one = '|'.join(game_field[0:3])
     line_two = '|'.join(game_field[3: 6])
@@ -5,8 +7,9 @@ def print_field(game_field):
     print(line_one)
     print(line_two)
     print(line_three)
+    print("\n")
 
-def player_move(cell, done_cell, game_field):
+def player_move(cell, done_cell, game_field, not_done_cell):
     if (done_cell.count(cell)!=0):
         print("Не читери!")
     elif cell< 0 or cell > 8:
@@ -14,8 +17,13 @@ def player_move(cell, done_cell, game_field):
     else:
         done_cell.append(cell)
         game_field[cell]="_o_"
+        not_done_cell.remove(cell)
 
-
+def computer_move(not_done_cell, game_field, done_cell):
+    cell = random.choice(not_done_cell)
+    done_cell.append(cell)
+    not_done_cell.remove(cell)
+    game_field[cell] = "_x_"
 
 
 def main():
@@ -23,10 +31,14 @@ def main():
     print_field(game_field)
     run = True
     done_cell = []
+    not_done_cell = [0,1,2,3,4,5,6,7,8]
     i = 0
     while run and i<10:
         cell = int(input("Куда вы хотите поставить о?\n"))
-        player_move(cell, done_cell, game_field)
+        player_move(cell, done_cell, game_field, not_done_cell)
+        print_field(game_field)
+        i+=1
+        computer_move(not_done_cell, game_field, done_cell)
         print_field(game_field)
         i+=1
 
