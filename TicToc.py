605eb1f2d7541c212fdf76ddaf71b28d8f5ef5f4
literale <43133944+literale@users.noreply.github.com>
2018-11-03 12:39:22 +0600

    Добавлена печать поля

--- TicToc.py
+++ TicToc.py
@@ -1,10 +1,18 @@
-
+def print_field(field):
+    line_one = '|'.join(field[0:3])
+    line_two = '|'.join(field[3: 6])
+    line_three = '|'.join(field[6: 9])
+    print(line_one)
+    print(line_two)
+    print(line_three)
 
 
 
 def main():
-    pass
+    game_field = ["_0_","_1_","_2_","_3_","_4_","_5_","_6_","_7_","_8_"]
+    print_field(game_field)
+    run = true
+    
 
 if __name__ == '__main__':
     main()
-    
